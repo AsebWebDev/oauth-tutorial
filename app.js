47977cf689a592262ccb80857e36993b4293fdfe
André Sebastian <a.sebastian@posteo.de>
2020-02-23 15:03:59 +0100

    add login page, setup routes in app.js

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const authRouter = require('./routes/auth-routes');
 
 const app = express();
 
+
+//set up view enginge
 app.set('view engine', 'ejs');
 
+// set up routes
+app.use('/auth', authRouter)
+
+// create home rout
 app.get('/', (req, res) => {
     res.render('home')
 });
